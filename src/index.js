153ed33ad314713b729e906ceb3a85deaa4aa0eb
Jack Paget <jackpaget1@gmail.com>
2022-10-22 18:57:34 +0100

    Changed router homepage

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider theme={theme}>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <HashRouter basename='/React'>
+    <HashRouter>
       <App />
     </HashRouter>
   </ChakraProvider>
